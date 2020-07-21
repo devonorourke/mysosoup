2390fd63842548bc0a636bc3379329d36d18a2a3
Devon O'Rourke <devon.orourke@gmail.com>
2020-07-21 13:01:08 -0400

    Update sequence_filtering.R

--- scripts/r_scripts/sequence_filtering.R
+++ scripts/r_scripts/sequence_filtering.R
@@ -56,8 +56,8 @@ sumry0 <- df %>%
   summarise(sumReads=sum(Reads), nASVs=n_distinct(ASVid))
 write.csv(sumry0, file = "~/Repos/mysosoup/data/text_tables/contam_evals/freqObs_and_seqDepth_perSample.csv",
           quote = FALSE, row.names = FALSE)
-  ## 5 of 7 negative control (NTC) samples have very low number of ASVs but fairly average numbers of reads per sample... most guano have >30 ASVs
-  ## only 2 of 7 true NTCs has more than 30, just like the "blankS39" sample which we KNOW had guano fall into the well
+## 5 of 7 negative control (NTC) samples have very low number of ASVs but fairly average numbers of reads per sample... most guano have >30 ASVs
+## only 2 of 7 true NTCs has more than 30, just like the "blankS39" sample which we KNOW had guano fall into the well
 
 ## related question: how many times do we see a particular ASV among any of the NTC samples?
 ## are those NTCs with few ASVs at least all showing the same ASVs?
@@ -67,9 +67,9 @@ NTConly_df <- df %>%
   filter(SampleType == "control") %>%
   group_by(ASVid) %>% 
   summarise(counts=n())
-  ## nope; we find that only 4/87 ASVs in four of eight samples
-  ## 5/87 ASVs in three samples; 15/87 in two samples
-  ## 63/87 in only single sample ... so most are unique, indicating that there isn't pervasive (ex. reagent) contamination
+## nope; we find that only 4/87 ASVs in four of eight samples
+## 5/87 ASVs in three samples; 15/87 in two samples
+## 63/87 in only single sample ... so most are unique, indicating that there isn't pervasive (ex. reagent) contamination
 
 
 ## Coloring the samples that were withing a cell of an NTC
@@ -83,8 +83,8 @@ ggplot(sumry0, aes(sumReads, nASVs, color=ContamArea, shape=SampleType)) +
   facet_wrap(~ Source) +
   scale_x_continuous(labels = comma) +
   theme_devon()
-  ## these results indicate that the kind of extraction (plate vs. isolate) isn't really diving differences in #ASVs or #reads per sample
-  ## location of true sample near a contaminated well doesn't influence number of reads or number of ASVs - they're indendent
+## these results indicate that the kind of extraction (plate vs. isolate) isn't really diving differences in #ASVs or #reads per sample
+## location of true sample near a contaminated well doesn't influence number of reads or number of ASVs - they're indendent
 
 
 
@@ -106,31 +106,7 @@ ggplot(sumry1, aes(sumReads, nASVs, color=ContamArea, shape=SampleType)) +
   facet_wrap(~ Source) +
   scale_x_continuous(labels = comma) +
   theme_devon()
-  ## results are similar to previous plot... filtering out singletons doesn't change overall patterns
-
-## drop any ASV that doesn't have at least Family-rank information (assumption here is poor classification MAY be indicative of sequence error)
-df_taxfilt <- df %>% 
-  filter(!is.na(kingdom_name)) %>% 
-  filter(!is.na(phylum_name)) %>% 
-  filter(!is.na(class_name)) %>% 
-  filter(!is.na(order_name)) %>% 
-  filter(!is.na(family_name)) %>% 
-  filter(phylum_name == "Arthropoda")
-sumry_taxfilt <- df_taxfilt %>% group_by(ASVid) %>% summarise(sumReads=sum(Reads), nSamples=n_distinct(SampleID))
-commonASVs_taxfilt <- intersect(df_taxfilt %>% filter(SampleType == "control") %>% select(ASVid),
-                                df_taxfilt %>% filter(SampleType == "sample") %>% select(ASVid)) %>% pull()
-
-unique_controlASVs_taxfilt <- setdiff(df_taxfilt %>% filter(SampleType == "control") %>% select(ASVid),
-                                df_taxfilt %>% filter(SampleType == "sample") %>% select(ASVid)) %>% pull()
-unique_sampleASVs_taxfilt <- setdiff(df_taxfilt %>% filter(SampleType == "sample") %>% select(ASVid),
-                                     df_taxfilt %>% filter(SampleType == "control") %>% select(ASVid)) %>% pull()
-
-## plot; save as 'contam_eval_ASV_and_SeqCounts_uniqnessColored_per_Guano_or_Control'
-ggplot(sumry_taxfilt, aes(x=sumReads, y=nSamples)) +
-  geom_point(data = sumry_taxfilt %>% filter(ASVid %in% commonASVs_taxfilt), color="black") +
-  geom_point(data = sumry_taxfilt %>% filter(ASVid %in% unique_sampleASVs_taxfilt), color="blue") +
-  geom_point(data = sumry_taxfilt %>% filter(ASVid %in% unique_controlASVs_taxfilt), color="red", size=2) +
-  theme_devon()
+## results are similar to previous plot... filtering out singletons doesn't change overall patterns
 
 
 ## what's up with those common ASVs in both true samples and contaminants?
@@ -160,34 +136,9 @@ light_filt_df <- merge(contam_sumry, noncontam_sumry)
 light_filt_df$more_ASVs <- light_filt_df$C_nSamples > light_filt_df$NC_nSamples  ## ZERO examples
 light_filt_df$more_Reads <- light_filt_df$C_nReads > light_filt_df$NC_nReads  ## Just 9 examples here
 
-  ## these filtering strategies suggest that despite some ASVs occurring in many samples, the ASVs that are shared between ..
-  ## .. contaminant and true samples rarely produce what you'd expect as indicative of widespread contamination: all NTCs having the most widely observed ASVs
-  ## Instead, we find that there are NEVER more than 4 NTCs with these common ASVs (just 4 of 79 ASVs); five examples where there are 3 NTCs with a common ASV; ..
-  ## .. and the remaining 70 ASVs are only ever detected in 2 or 1 NTC samples.
+## these filtering strategies suggest that despite some ASVs occurring in many samples, the ASVs that are shared between ..
+## .. contaminant and true samples rarely produce what you'd expect as indicative of widespread contamination: all NTCs having the most widely observed ASVs
+## Instead, we find that there are NEVER more than 4 NTCs with these common ASVs (just 4 of 79 ASVs); five examples where there are 3 NTCs with a common ASV; ..
+## .. and the remaining 70 ASVs are only ever detected in 2 or 1 NTC samples.
 
-## we can further refind this by highlight just the ASVs which would be used in downstream analyses for diet work: ..
-## .. those ASVs that contain Arthropod phylum-classification, and at least Family-rank information:
-
-taxfilt_contamASVs <- intersect(light_filt_df$ASVid, df_taxfilt$ASVid)
-contam_taxfilt_df <- light_filt_df %>% filter(ASVid %in% taxfilt_contamASVs)
-
-  ## this further reduces our analyses to just 68 ASVs
-
-## We'll create two lists of ASVs to filter our original dataset:
-## 1) ASVs that meet our taxonomy completeness criteria (Arthropod-associated phylum_name with at least Family info) and include all ASVs whether or not they are present in NTC samples
-## 2) As with #1 above, but removing the 68 ASVs present in NTC samples 
-
-taxfilt_ASVs <- df_taxfilt %>% distinct(ASVid)
-colnames(taxfilt_ASVs)[1] <- "#OTUID"
-write.table(taxfilt_ASVs, "~/Repos/mysosoup/data/taxonomy/taxfiltd_ASVs_NTCincluded.txt", row.names = FALSE, quote = FALSE, col.names = TRUE)
-
-taxfilt_2_ASVs <- df_taxfilt %>% filter(!ASVid %in% contam_df$ASVid) %>% distinct(ASVid)
-colnames(taxfilt_2_ASVs)[1] <- "#OTUID"
-write.table(taxfilt_2_ASVs, "~/Repos/mysosoup/data/taxonomy/taxfiltd_ASVs_NTCdrops.txt", row.names = FALSE, quote = FALSE, col.names = TRUE)
-
-## These two text files are used to filter by retaining only the ASVs listed in those files. 
-## The resulting `.qza` artifacts are then rarefied and then used for diversity analyses.
-
-## double check to ensure there are NTCs (or not) in these two filtered ASV sets:
-tmp_wNTCasv_df <- df %>% filter(ASVid %in% taxfilt_ASVs$ASVid)
-tmp_noNTCasv_df <- df %>% filter(ASVid %in% taxfilt_2_ASVs$ASVid)
+## final decision is to just drop any NTC sample, and any sequence features associated exclusively with that group
